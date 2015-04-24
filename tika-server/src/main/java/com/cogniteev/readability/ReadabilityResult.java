f1ba7f5634647e4fcd8d03f9bb25623074a2ef61
Philippe David <philippe@cogniteev.com>
2015-04-24 18:14:10 +0200

    Add og and twitter attributes (cherry picked from commit 76020ff)

--- tika-server/src/main/java/com/cogniteev/readability/ReadabilityResult.java
+++ tika-server/src/main/java/com/cogniteev/readability/ReadabilityResult.java
@@ -3,8 +3,28 @@ package com.cogniteev.readability;
 import de.jetwick.snacktory.JResult;
 
 import javax.xml.bind.annotation.XmlRootElement;
+import java.util.Map;
 
 @XmlRootElement
 public class ReadabilityResult extends JResult {
+    public Map<String, String> getOgAttributes() {
+        return ogAttributes;
+    }
+
+    public void setOgAttributes(Map<String, String> ogAttributes) {
+        this.ogAttributes = ogAttributes;
+    }
+
+    public Map<String, String> ogAttributes;
+
+    public Map<String, String> getTwitterAttributes() {
+        return twitterAttributes;
+    }
+
+    public void setTwitterAttributes(Map<String, String> twitterAttributes) {
+        this.twitterAttributes = twitterAttributes;
+    }
+
+    public Map<String, String> twitterAttributes;
 
 }
