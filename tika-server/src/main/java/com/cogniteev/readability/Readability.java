82c09d230aad07206aa40769f205dde4e146eb66
Philippe David <philippe@cogniteev.com>
2015-04-24 18:14:10 +0200

    Add og and twitter attributes (cherry picked from commit 76020ff)

--- tika-server/src/main/java/com/cogniteev/readability/Readability.java
+++ tika-server/src/main/java/com/cogniteev/readability/Readability.java
@@ -1,14 +1,22 @@
 package com.cogniteev.readability;
 
 import de.jetwick.snacktory.ArticleTextExtractor;
+import de.jetwick.snacktory.OutputFormatter;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Readability {
+
+
 
     @Path("/readability")
     @PUT
@@ -17,9 +25,30 @@ public class Readability {
     public ReadabilityResult readability(String source) throws Exception {
         ArticleTextExtractor extractor = new ArticleTextExtractor();
         ReadabilityResult res = new ReadabilityResult();
-        extractor.extractContent(res, source);
+        Document doc = Jsoup.parse(source);
+        extractor.extractContent(res, doc, new OutputFormatter());
+
+        res.setOgAttributes(extractOgAttributes(doc));
+        res.setTwitterAttributes(extractTwitterAttributes(doc));
+
         return res;
     }
 
+    protected Map<String, String> extractOgAttributes(Document doc) {
+        Map<String, String> res = new HashMap<String, String>();
+        for(Element e: doc.select("head meta[property^=og:]")) {
+            res.put(e.attr("property"), e.attr("content"));
+        }
 
+        return res;
+    }
+
+    protected Map<String, String> extractTwitterAttributes(Document doc) {
+        Map<String, String> res = new HashMap<String, String>();
+        for(Element e: doc.select("head meta[name^=twitter:]")) {
+            res.put(e.attr("name"), e.attr("content"));
+        }
+
+        return res;
+    }
 }
